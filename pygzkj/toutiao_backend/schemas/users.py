538b4f42691529eb2f0fdbab716cb0ed696cf7f8
203522 <17194714+m1ys@user.noreply.gitee.com>
2026-09-10 23:07:01 +0800

    第三十七天学习，关于完成了相关的user模块，生日后几天心情低落

--- pygzkj/toutiao_backend/schemas/users.py
+++ pygzkj/toutiao_backend/schemas/users.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
+from starlette.responses import JSONResponse
 
 
 class UserRequest(BaseModel):
@@ -22,3 +24,8 @@ class UpdatePassword(BaseModel):
     username: str
     old_password: str
     new_password: str
+
+def success_response(massage: str='Success!',date=None):
+    content = {"code":200,"msg":massage,date:date}
+    #把对象转换成json数据结构输出
+    return JSONResponse(content=jsonable_encoder(content))
